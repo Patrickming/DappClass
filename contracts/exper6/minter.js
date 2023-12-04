c96623bb14ca15b751de9f45f0264328a08a4343
Patrickming <patrickmingx@gmail.com>
2023-12-04 10:42:53 +0800

    fix exper6

--- contracts/exper6/minter.js
+++ contracts/exper6/minter.js
@@ -1,8 +1,9 @@
-import {ethers, JsonRpcProvider} from 'ethers'
+import { ethers, JsonRpcProvider } from 'ethers'
 import fs from 'fs'
 
-async function main(address , uri) {
+async function main(address, uri) {
     const provider = new JsonRpcProvider(''
     )
     const signer = await provider.getSigner()
-    const abi = JSON.parse(fs.readFileSync("./abis/MyNFT.json, 'utf8'")) 
+    const abi = JSON.parse(fs.readFileSync("./abis/MyNFT.json, 'utf8'"))
+}
